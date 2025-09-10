10ee35df07969e6c481a6bef5142adf73249473a
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-10 16:37:32 +0530

    [jayesh tapdiya]: updated the auth Middelware file  and added some new thing in the body which will help in notes section : username,emailid, userid .

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,35 +1,41 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import HttpStatus from 'http-status-codes';
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 
-/**
- * Middleware to authenticate if user has a valid Authorization token
- * Authorization: Bearer <token>
- *
- * @param {Object} req
- * @param {Object} res
- * @param {Function} next
- */
-export const userAuth = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  try {
-    let bearerToken = req.header('Authorization');
-    if (!bearerToken)
-      throw {
-        code: HttpStatus.BAD_REQUEST,
-        message: 'Authorization token is required'
-      };
-    bearerToken = bearerToken.split(' ')[1];
+interface UserDetails {
+  user_id: string;
+  username: string;
+  email: string;
+}
 
-    const { user }: any = await jwt.verify(bearerToken, 'your-secret-key');
-    res.locals.user = user;
-    res.locals.token = bearerToken;
-    next();
-  } catch (error) {
-    next(error);
-  }
+export const userAuth = (secretKey: string) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let bearerToken = req.header('Authorization');
+      if (!bearerToken)
+        throw {
+          code: HttpStatus.BAD_REQUEST,
+          message: 'Authorization token is required'
+        };
+      bearerToken = bearerToken.split(' ')[1];
+
+      let userDetails = jwt.verify(bearerToken, secretKey) as JwtPayload &
+        UserDetails;
+
+      req.body.createdBy = userDetails.user_id;
+      req.body.username = userDetails.username;
+      req.body.Email = userDetails.email;
+      next();
+    } catch (error) {
+      if (error instanceof Error) {
+        return next({
+          code: HttpStatus.UNAUTHORIZED,
+          message: 'Invalid or expired token',
+          error: error.message
+        });
+      }
+      return next(error);
+    }
+  };
 };
