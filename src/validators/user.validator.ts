f587f598a5bba791f59464972fa02ec6f29c05f7
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-10 14:12:59 +0530

    [jayesh tapdiya] updated the user validator  for the user signup functionality in fundonotes by typescript

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -4,7 +4,13 @@ import { Request, Response, NextFunction } from 'express';
 class UserValidator {
   public newUser = (req: Request, res: Response, next: NextFunction): void => {
     const schema = Joi.object({
-      name: Joi.string().min(4).required()
+      fname: Joi.string().min(1).required(),
+      lname: Joi.string().min(1).required(),
+      email: Joi.string().email().required(),
+      password: Joi.string()
+        .min(8)
+        .pattern(new RegExp('^[a-zA-Z0-9@]{3,30}$'))
+        .required()
     });
     const { error } = schema.validate(req.body);
     if (error) {
