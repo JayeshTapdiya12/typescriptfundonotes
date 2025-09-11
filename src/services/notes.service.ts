901846519f0de4a5f0b954ab45b841e0a0e522a1
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-11 21:34:28 +0530

    [jayesh tapdiya] update the service file for adding the functionality of the updating the note in fundonotes

--- src/services/notes.service.ts
+++ src/services/notes.service.ts
@@ -161,5 +161,37 @@ class NoteService {
       };
     }
   };
+
+  public updateNote = async (
+    body,
+    id
+  ): Promise<INotes[] | INotesSuccess | INoteError | INoteNotFound> => {
+    try {
+      const data = await Notes.findOne({ createdBy: body.createdBy, _id: id });
+      if (!data) {
+        return {
+          code: 400,
+          message: 'note did not exisit',
+          success: false
+        };
+      } else {
+        await data.update({ ...body });
+        await data.save();
+        return {
+          code: 200,
+          message: `Note has been updated successfully`,
+          data: data,
+          success: true
+        };
+      }
+    } catch (error) {
+      return {
+        code: 500,
+        message: 'Internal server error',
+        error: error.message,
+        success: false
+      };
+    }
+  };
 }
 export default NoteService;
