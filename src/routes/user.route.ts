abf907563636f1d716c17004e0d69031716627f1
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-11 10:11:13 +0530

    [jayesh tapdiya] update the route file for the reset password and add the reset password validator

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,6 +2,7 @@ import express, { IRouter } from 'express';
 import userController from '../controllers/user.controller';
 import userValidator from '../validators/user.validator';
 import { userAuth } from '../middlewares/auth.middleware';
+import ResetPasswordValidator from '../validators/resetpasswordvalidator';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,6 +10,7 @@ class UserRoutes {
   private UserController = new userController();
   private router = express.Router();
   private UserValidator = new userValidator();
+  private ResetPasswordValidator = new ResetPasswordValidator();
 
   constructor() {
     this.routes();
@@ -31,6 +33,13 @@ class UserRoutes {
       '/forget_password',
       userAuth(process.env.jwt_sceret_key),
       this.UserController.forgetpassword
+    );
+
+    this.router.post(
+      '/reser_password',
+      userAuth(process.env.jwt_sceret_key),
+      this.ResetPasswordValidator.newPassword,
+      this.UserController.resetPassword
     );
   };
 
