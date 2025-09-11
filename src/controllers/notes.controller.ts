ee133f63d19112a3507d946fa36cdd9ecf3564c8
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-11 16:55:26 +0530

    [jayesh tapdiya] udpate the controller file for adding the archiving the notes functionality in the fundonotes

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -77,6 +77,28 @@ class NoteController {
       });
     }
   };
+
+  public archived = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data = await this.noteService.archived(req.body, req.params._id);
+      if ((data as INotesSuccess).code) {
+        res.status((data as INotesSuccess).code).json(data);
+      } else if ((data as INoteNotFound).code) {
+        res.status((data as INoteNotFound).code).json(data);
+      } else {
+        res.status((data as INoteError).code).json(data);
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: 'An error occurred during archiving   notes  of the user',
+        error: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  };
 }
 
 export default NoteController;
