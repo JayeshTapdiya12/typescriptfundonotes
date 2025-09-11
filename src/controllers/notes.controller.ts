62d596b2fd624c12b6829fbfb8405942dd6a0189
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-11 21:45:14 +0530

    [jayesh tapdiya] update the contorller file for adding the functionality of the  updating color of the note in fundonotes

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -145,6 +145,29 @@ class NoteController {
       });
     }
   };
+
+  public color = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data = await this.noteService.color(req.body, req.params._id);
+
+      if ((data as INotesSuccess).code) {
+        res.status((data as INotesSuccess).code).json(data);
+      } else if ((data as INoteNotFound).code) {
+        res.status((data as INoteNotFound).code).json(data);
+      } else {
+        res.status((data as INoteError).code).json(data);
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: 'An error occurred during color the note  of the user',
+        error: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  };
 }
 
 export default NoteController;
