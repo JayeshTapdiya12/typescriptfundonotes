3b78d94d5ceb953b2e13779fb13aabce540e3b3c
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-11 21:50:01 +0530

    [jayesh tapdiya] update the controller file for adding the functionality of the deleting the note in fundonotes

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -168,6 +168,29 @@ class NoteController {
       });
     }
   };
+
+  public deleteNote = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data = await this.noteService.deleteNote(req.body, req.params._id);
+
+      if ((data as INotesSuccess).code) {
+        res.status((data as INotesSuccess).code).json(data);
+      } else if ((data as INoteNotFound).code) {
+        res.status((data as INoteNotFound).code).json(data);
+      } else {
+        res.status((data as INoteError).code).json(data);
+      }
+    } catch (error) {
+      res.status(500).json({
+        message: 'An error occurred during deleting the note  of the user',
+        error: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  };
 }
 
 export default NoteController;
