076e8b87c6b47059b2929853ed80239da05a32c2
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-12 10:48:12 +0530

    [jayesh tapdiya]: update the controller file for adding the udpate label functionality in fundonotes

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -240,6 +240,30 @@ class NoteController {
       });
     }
   };
+
+  public updateLabel = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data = await this.noteService.updateLabel(req.body);
+
+      if ((data as INotesSuccess).code) {
+        res.status((data as INotesSuccess).code).json(data);
+      } else if ((data as INoteNotFound).code) {
+        res.status((data as INoteNotFound).code).json(data);
+      } else {
+        res.status((data as INoteError).code).json(data);
+      }
+    } catch (error) {
+      res.status(500).json({
+        message:
+          'An error occurred during updating  the label of  the note  of the user',
+        error: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  };
 }
 
 export default NoteController;
