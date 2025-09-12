6024746e020fec19458645ca8c929dab7d9fdcdd
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-12 10:54:55 +0530

    [jayesh tapdiya]: update the contoller file for adding the delete label functionality in fundonotes

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -264,6 +264,29 @@ class NoteController {
       });
     }
   };
+  public deletelabel = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data = await this.noteService.deletelabel(req.body, req.params._id);
+
+      if ((data as INotesSuccess).code) {
+        res.status((data as INotesSuccess).code).json(data);
+      } else if ((data as INoteNotFound).code) {
+        res.status((data as INoteNotFound).code).json(data);
+      } else {
+        res.status((data as INoteError).code).json(data);
+      }
+    } catch (error) {
+      res.status(500).json({
+        message:
+          'An error occurred during deleting  the label of  the note  of the user',
+        error: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  };
 }
 
 export default NoteController;
