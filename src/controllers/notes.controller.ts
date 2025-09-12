ce6c4ce6861fcc4a5b7566ecf7997220f3b145b6
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-12 11:45:02 +0530

    [jayesh tapdiya]: update the controller file for adding the add remincer functionality in fundonotes

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -289,6 +289,7 @@ class NoteController {
   };
 
   // reminder starts from here
+
   public getreminder = async (
     req: Request,
     res: Response,
@@ -308,6 +309,29 @@ class NoteController {
       res.status(500).json({
         message:
           'An error occurred during getting   the reminder of  the note  of the user',
+        error: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  };
+  public addreminder = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data = await this.noteService.addreminder(req.body, req.params._id);
+
+      if ((data as INotesSuccess).code) {
+        res.status((data as INotesSuccess).code).json(data);
+      } else if ((data as INoteNotFound).code) {
+        res.status((data as INoteNotFound).code).json(data);
+      } else {
+        res.status((data as INoteError).code).json(data);
+      }
+    } catch (error) {
+      res.status(500).json({
+        message:
+          'An error occurred during adding   the reminder of  the note  of the user',
         error: error instanceof Error ? error.message : 'Unknown error'
       });
     }
