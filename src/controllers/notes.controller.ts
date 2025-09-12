c81394ab087351cea82a41eba1f44d96c7ac5645
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-12 10:21:42 +0530

    [jayesh tapdiya]: update the controller file for adding the get label functionality in fundonotes

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -191,6 +191,32 @@ class NoteController {
       });
     }
   };
+
+  // labels :
+
+  public getlabel = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data = await this.noteService.getlabel(req.body);
+
+      if ((data as INotesSuccess).code) {
+        res.status((data as INotesSuccess).code).json(data);
+      } else if ((data as INoteNotFound).code) {
+        res.status((data as INoteNotFound).code).json(data);
+      } else {
+        res.status((data as INoteError).code).json(data);
+      }
+    } catch (error) {
+      res.status(500).json({
+        message:
+          'An error occurred during getting the label of  the note  of the user',
+        error: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  };
 }
 
 export default NoteController;
