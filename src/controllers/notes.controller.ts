11eda892a1b73624ad1b97b5d8224d68fb03ecef
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-12 11:48:35 +0530

    [jayesh tapdiya]: update the contoller file for adding the udpate reminder functionality in fundonotes

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -336,6 +336,33 @@ class NoteController {
       });
     }
   };
+
+  public updateReminder = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data = await this.noteService.updateReminder(
+        req.body,
+        req.params._id
+      );
+
+      if ((data as INotesSuccess).code) {
+        res.status((data as INotesSuccess).code).json(data);
+      } else if ((data as INoteNotFound).code) {
+        res.status((data as INoteNotFound).code).json(data);
+      } else {
+        res.status((data as INoteError).code).json(data);
+      }
+    } catch (error) {
+      res.status(500).json({
+        message:
+          'An error occurred during updating   the reminder of  the note  of the user',
+        error: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  };
 }
 
 export default NoteController;
