13de3b3529ef8dbcb0cc3081c3751253e3a74127
JAYESHTAPDIYA12 <jayesh121220@gmail.com>
2025-09-12 12:28:23 +0530

    [jayesh tapdiya]: update the controller file for adding the delete collaborators  functionality in fundonotes

--- src/controllers/notes.controller.ts
+++ src/controllers/notes.controller.ts
@@ -418,6 +418,33 @@ class NoteController {
       });
     }
   };
+  public deleteCollaborators = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    try {
+      const data = await this.noteService.deleteCollaborators(
+        req.body,
+        req.params._id
+      );
+
+      if ((data as INotesSuccess).code) {
+        res.status((data as INotesSuccess).code).json(data);
+      } else if ((data as INoteNotFound).code) {
+        res.status((data as INoteNotFound).code).json(data);
+      } else {
+        res.status((data as INoteError).code).json(data);
+      }
+    } catch (error) {
+      res.status(500).json({
+        message:
+          'An error occurred during deleting the collaborators of  the note  of the user',
+        error: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  };
+
   public addCollaborators = async (
     req: Request,
     res: Response,
